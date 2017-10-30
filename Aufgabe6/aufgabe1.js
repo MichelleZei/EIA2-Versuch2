a029676542443f8ec9803bf5541d74a1a12395c0
Sabine <Sabine@Sabine-NB.mshome.net>
2017-10-30 15:44:22 +0100

    Aufgabenordner 1-12

---
+++ Aufgabe6/aufgabe1.js
@@ -0,0 +1,9 @@
+/*
+Aufgabe: Aufgabe 1 - Skipiste
+Name: Michelle Zei�ner
+Matrikel:
+Datum: 30.10.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/ 
+//# sourceMappingURL=aufgabe1.js.map
